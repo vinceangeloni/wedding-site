6ee53e5f0ba9cb5a5a4fcc5fc648daf09ab7d6ff
Vincent Angeloni <vinceangeloni@AC-MacBook-Pro.local>
2017-05-07 14:33:52 -0400

    Fix for RSVP code generation

--- app/models/invite.rb
+++ app/models/invite.rb
@@ -6,6 +6,6 @@ class Invite < ActiveRecord::Base
 
 	# Generates the rsvp code
 	def generate_code
-	      self.rsvp_code = SecureRandom.random_number(36**4).to_s(36).rjust(4, "0").to
+	      self.rsvp_code = SecureRandom.random_number(36**4).to_s(36).rjust(4, "0").to_s
   	end
 end
