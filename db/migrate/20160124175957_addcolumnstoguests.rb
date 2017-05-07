3d57a4f7bc9d9f255113278b9340fbcec24eabe7
Vincent Angeloni <vinceangeloni@AC-MacBook-Pro.local>
2017-05-07 13:50:54 -0400

    Migration update

--- db/migrate/20160124175957_addcolumnstoguests.rb
+++ db/migrate/20160124175957_addcolumnstoguests.rb
@@ -1,7 +1,5 @@
 class Addcolumnstoguests < ActiveRecord::Migration
   def change
   	add_column :guests, :num_attending, :integer, :null => false, :default => 1
-  	add_column :guests, :menu_item, :integer, :null => false, :default => 1
-  	add_column :guests, :dietary, :boolean, :default => false
   end
 end
