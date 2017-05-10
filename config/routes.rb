9308601b5d59da84f16242c2b8a5c791bf6f4912
Vincent Angeloni <vinceangeloni@AC-MacBook-Pro.local>
2017-05-10 09:03:57 -0400

    Remove sign up

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
     resources :passwords, controller: 'authenticate/passwords', only: [:edit, :update]
   end
 
-  get '/sign_up', to: 'authenticate/users#new', as: 'sign_up'
+  #get '/sign_up', to: 'authenticate/users#new', as: 'sign_up'
   get '/sign_in', to: 'authenticate/sessions#new', as: 'sign_in'
   get '/sign_out', to: 'authenticate/sessions#destroy', as: 'sign_out'
 
